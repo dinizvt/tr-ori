c7485d336c5843a1360471fa54c65edf29bebc83
Lucashm <lucasmarchiori12@hotmail.com>
2021-10-20 22:43:28 -0300

    menuzinho

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
-
+#include <stdlib.h>
 #include "arquivo.h"
 #include "pessoa.h"
 
@@ -16,9 +16,65 @@ void writeUntilStop (Arquivo* f) {
     } while (p != NULL);
 }
 
+void menu(Arquivo *f)
+{
+    Pessoa* p;
+    bool loop = true;
+    int escolha;
+    while(loop)
+    {
+        system("cls");
+        cout << "Escolha o que deseja: " << endl;
+        cout << "1-) Inserir pessoas " << endl;
+        cout << "2-) Excluir pessoas: " << endl;
+        cout << "3-)Procurar por key: " << endl;
+        cout << "4-) Procurar por ordem de inseção" << endl;
+        cout << "5-)Procurar por nome" << endl;
+        cout << "0-) Para sair" << endl;
+        cin >> escolha;
+
+        switch (escolha)
+        {
+        case 1:
+            writeUntilStop(f);
+            break;
+        case 2:
+            int key;
+            cout << " qual a key que gostaria de excluir? ";
+            cin >> key;
+            f->rm(key);
+            break;
+        
+        case 3:
+            int key;
+            cout << " qual a key que gostaria de buscar? ";
+            cin >> key; 
+            p = f->readByKey(key);
+            printRecord(p);
+            break;
+        
+        case 4:
+            int n;
+            cout << " qual a 'ordem de inserção' que gostaria de buscar? ";
+            cin >> n; 
+            p = f->readByN(n);
+            printRecord(p);
+
+            break;
+        case 5:
+            break;
+        
+        
+        default:
+            break;
+        }
+    }
+
+
+}
+
 int main (int argc, char** argv) {
     Arquivo* f = new Arquivo("teste");
-    writeUntilStop(f);
-    f->rm(2);
-    writeUntilStop(f);
+    menu(f);
+    return 0;
 }
